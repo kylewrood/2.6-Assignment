1997d183baf208b10bcd1b8c00705f2615ed116d
kylewrood <137108769+kylewrood@users.noreply.github.com>
2023-07-28 18:49:27 -0500

    Submitting my 2.6 assignment

---
+++ JS/index.js
@@ -0,0 +1,22 @@
+function quad(){
+
+// program to solve quadratic equation
+let root1, root2;
+
+// take input from the user
+let a = prompt("Enter the first number: ");
+let b = prompt("Enter the second number: ");
+let c = prompt("Enter the third number: ");
+
+// calculate discriminant
+let discriminant = b * b - 4 * a * c;
+
+// condition for real and different roots
+if (discriminant > 0) {
+    root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+    // result
+    document.getElementById("quadResult").innerHTML= root1 + root2;
+}
+}
